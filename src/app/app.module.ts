1180978a1ed1154cc52821b41ded49fe26582da7
Stokris <kristen.stokes@me.com>
2018-12-05 13:27:39 -0500

    home styling in app component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,10 +4,28 @@ import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { MatButtonModule, MatCheckboxModule } from '@angular/material';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HomeComponent } from './home/home.component';
+import { ShopComponent } from './shop/shop.component';
+import { ContactComponent } from './contact/contact.component';
+import { HelpFaqComponent } from './help-faq/help-faq.component';
+import { AuthComponent } from './auth/auth.component';
+import { ProfileComponent } from './profile/profile.component';
+import { CartComponent } from './cart/cart.component';
+import { LoginSignupComponent } from './login-signup/login-signup.component';
+import { NavComponent } from './nav/nav.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent,
+    ShopComponent,
+    ContactComponent,
+    HelpFaqComponent,
+    AuthComponent,
+    ProfileComponent,
+    CartComponent,
+    LoginSignupComponent,
+    NavComponent
   ],
   imports: [
     BrowserModule,
