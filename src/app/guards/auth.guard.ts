7ddf9fe47fea43c7f4ac2b56a44b1be8e6ec5523
Cory Crawford <coryc.vector@gmail.com>
2018-12-16 13:44:49 -0500

    made components folder

--- src/app/guards/auth.guard.ts
+++ src/app/guards/auth.guard.ts
@@ -1,4 +1,4 @@
-import { Injectable } from '@angular/core';
-import { CanActivate, Router } from '@angular/router';
-import 'rxjs/add/operator/map';
-import { Observable } from 'rxjs/Rx';
+// import { Injectable } from '@angular/core';
+// import { CanActivate, Router } from '@angular/router';
+// import 'rxjs/add/operator/map';
+// import { Observable } from 'rxjs/Rx';
