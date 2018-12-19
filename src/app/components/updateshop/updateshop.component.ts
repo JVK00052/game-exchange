a8a3c6c742737d7de309df7d3ff903919cf99f6c
Cory Crawford <coryc.vector@gmail.com>
2018-12-19 08:08:45 -0500

    added folders

---
+++ src/app/components/updateshop/updateshop.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-updateshop',
+  templateUrl: './updateshop.component.html',
+  styleUrls: ['./updateshop.component.css']
+})
+export class UpdateshopComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
